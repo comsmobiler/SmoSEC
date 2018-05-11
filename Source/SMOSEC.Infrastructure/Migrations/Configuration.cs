abbd7d76e95c2f47cbf11e22d7d0ab99738eee83
Steven <Steven.Lu@smobiler.com>
2018-05-11 14:19:20 +0800

    数据库生成数据更新

--- Source/SMOSEC.Infrastructure/Migrations/Configuration.cs
+++ Source/SMOSEC.Infrastructure/Migrations/Configuration.cs
@@ -29,25 +29,41 @@ namespace SMOSEC.Infrastructure.Migrations
             //    );
             //
             context.coreUsers.AddOrUpdate(
-                new coreUser { USER_ID = "13123456789", USER_ADDRESS = "嘉兴", USER_BIRTHDAY = DateTime.Now, USER_NAME = "富贵", USER_ROLE = "ADMIN", USER_PASSWORD = "123456", USER_LOCATIONID = "01" },
-                new coreUser { USER_ID = "12345678912", USER_ADDRESS = "嘉兴", USER_BIRTHDAY = DateTime.Now, USER_NAME = "张三", USER_ROLE = "ADMIN", USER_PASSWORD = "123456", USER_LOCATIONID = "01" },
-                new coreUser { USER_ID = "12345678913", USER_ADDRESS = "上海", USER_BIRTHDAY = DateTime.Now, USER_NAME = "李四", USER_ROLE = "SMOSECAdmin", USER_PASSWORD = "123456", USER_LOCATIONID = "02" },
-                new coreUser { USER_ID = "12345678914", USER_ADDRESS = "郑州", USER_BIRTHDAY = DateTime.Now, USER_NAME = "王五", USER_ROLE = "SMOSECAdmin", USER_PASSWORD = "123456", USER_LOCATIONID = "03" },
-                new coreUser { USER_ID = "12345678915", USER_ADDRESS = "嘉兴", USER_BIRTHDAY = DateTime.Now, USER_NAME = "赵二", USER_ROLE = "ADMIN", USER_PASSWORD = "123456", USER_LOCATIONID = "01" },
-                new coreUser { USER_ID = "12345678916", USER_ADDRESS = "上海", USER_BIRTHDAY = DateTime.Now, USER_NAME = "老钱", USER_ROLE = "SMOSECUser", USER_PASSWORD = "123456", USER_LOCATIONID = "02" },
-                new coreUser { USER_ID = "12345678917", USER_ADDRESS = "嘉兴", USER_BIRTHDAY = DateTime.Now, USER_NAME = "李狗蛋", USER_ROLE = "SMOSECUser", USER_PASSWORD = "123456", USER_LOCATIONID = "01", USER_ISDEMO = 1 }
+                new coreUser { USER_ID = "13123456789", USER_ADDRESS = "嘉兴", USER_BIRTHDAY = DateTime.Now, USER_NAME = "富贵", USER_ROLE = "ADMIN", USER_PASSWORD = "123456", USER_LOCATIONID = "01", USER_SEX = 0 },
+                new coreUser { USER_ID = "12345678912", USER_ADDRESS = "嘉兴", USER_BIRTHDAY = DateTime.Now, USER_NAME = "张三", USER_ROLE = "ADMIN", USER_PASSWORD = "123456", USER_LOCATIONID = "01", USER_SEX = 0 },
+                new coreUser { USER_ID = "12345678913", USER_ADDRESS = "上海", USER_BIRTHDAY = DateTime.Now, USER_NAME = "李四", USER_ROLE = "SMOSECAdmin", USER_PASSWORD = "123456", USER_LOCATIONID = "02", USER_SEX = 0 },
+                new coreUser { USER_ID = "12345678914", USER_ADDRESS = "郑州", USER_BIRTHDAY = DateTime.Now, USER_NAME = "王五", USER_ROLE = "SMOSECAdmin", USER_PASSWORD = "123456", USER_LOCATIONID = "03", USER_SEX = 0 },
+                new coreUser { USER_ID = "12345678915", USER_ADDRESS = "嘉兴", USER_BIRTHDAY = DateTime.Now, USER_NAME = "赵二", USER_ROLE = "ADMIN", USER_PASSWORD = "123456", USER_LOCATIONID = "01", USER_SEX = 0 },
+                new coreUser { USER_ID = "12345678916", USER_ADDRESS = "上海", USER_BIRTHDAY = DateTime.Now, USER_NAME = "老钱", USER_ROLE = "SMOSECUser", USER_PASSWORD = "123456", USER_LOCATIONID = "02", USER_SEX = 0 },
+                new coreUser { USER_ID = "12345678917", USER_ADDRESS = "嘉兴", USER_BIRTHDAY = DateTime.Now, USER_NAME = "李狗蛋", USER_ROLE = "SMOSECUser", USER_PASSWORD = "123456", USER_LOCATIONID = "01", USER_ISDEMO = 1, USER_SEX = 0 }
                 );
             context.AssetsTypes.AddOrUpdate(
-                new AssetsType { TYPEID = "01", EXPIRYDATE = 8, EXPIRYDATEUNIT = 1, ISENABLE = 1, NAME = "电脑", TLEVEL = 1, CREATEUSER = "12345678912" },
-                new AssetsType { TYPEID = "02", EXPIRYDATE = 9, EXPIRYDATEUNIT = 1, ISENABLE = 1, NAME = "汽车", TLEVEL = 1, CREATEUSER = "12345678912" },
-                new AssetsType { TYPEID = "03", EXPIRYDATE = 10, EXPIRYDATEUNIT = 1, ISENABLE = 1, NAME = "显示器", TLEVEL = 2, PARENTTYPEID = "01", CREATEUSER = "12345678912" },
-                new AssetsType { TYPEID = "04", EXPIRYDATE = 11, EXPIRYDATEUNIT = 1, ISENABLE = 1, NAME = "饮料", TLEVEL = 2, PARENTTYPEID = "01", CREATEUSER = "12345678912" },
-                new AssetsType { TYPEID = "05", EXPIRYDATE = 12, EXPIRYDATEUNIT = 1, ISENABLE = 1, NAME = "鼠标", TLEVEL = 3, PARENTTYPEID = "03", CREATEUSER = "12345678912" },
-                new AssetsType { TYPEID = "06", EXPIRYDATE = 13, EXPIRYDATEUNIT = 1, ISENABLE = 1, NAME = "空调", TLEVEL = 3, PARENTTYPEID = "03", CREATEUSER = "12345678912" }
+                new AssetsType { TYPEID = "01", EXPIRYDATE = 8, EXPIRYDATEUNIT = 1, ISENABLE = 1, NAME = "电脑整机", TLEVEL = 1, CREATEUSER = "12345678912" },
+                new AssetsType { TYPEID = "02", EXPIRYDATE = 8, EXPIRYDATEUNIT = 1, ISENABLE = 1, NAME = "电脑配件", TLEVEL = 1, CREATEUSER = "12345678912" },
+                new AssetsType { TYPEID = "03", EXPIRYDATE = 8, EXPIRYDATEUNIT = 1, ISENABLE = 1, NAME = "电脑外设", TLEVEL = 1, CREATEUSER = "12345678912" },
+                new AssetsType { TYPEID = "04", EXPIRYDATE = 8, EXPIRYDATEUNIT = 1, ISENABLE = 1, NAME = "网络硬件", TLEVEL = 1, CREATEUSER = "12345678912" },
+                new AssetsType { TYPEID = "05", EXPIRYDATE = 9, EXPIRYDATEUNIT = 1, ISENABLE = 1, NAME = "办公设备", TLEVEL = 1, CREATEUSER = "12345678912" },
+                new AssetsType { TYPEID = "06", EXPIRYDATE = 10, EXPIRYDATEUNIT = 1, ISENABLE = 1, NAME = "笔记本", TLEVEL = 2, PARENTTYPEID = "01", CREATEUSER = "12345678912" },
+                new AssetsType { TYPEID = "07", EXPIRYDATE = 10, EXPIRYDATEUNIT = 1, ISENABLE = 1, NAME = "台式机", TLEVEL = 2, PARENTTYPEID = "01", CREATEUSER = "12345678912" },
+                new AssetsType { TYPEID = "08", EXPIRYDATE = 10, EXPIRYDATEUNIT = 1, ISENABLE = 1, NAME = "平板", TLEVEL = 2, PARENTTYPEID = "01", CREATEUSER = "12345678912" },
+                new AssetsType { TYPEID = "09", EXPIRYDATE = 10, EXPIRYDATEUNIT = 1, ISENABLE = 1, NAME = "显示器", TLEVEL = 2, PARENTTYPEID = "02", CREATEUSER = "12345678912" },
+                new AssetsType { TYPEID = "10", EXPIRYDATE = 10, EXPIRYDATEUNIT = 1, ISENABLE = 1, NAME = "固态硬盘", TLEVEL = 2, PARENTTYPEID = "02", CREATEUSER = "12345678912" },
+                new AssetsType { TYPEID = "11", EXPIRYDATE = 10, EXPIRYDATEUNIT = 1, ISENABLE = 1, NAME = "硬盘", TLEVEL = 2, PARENTTYPEID = "02", CREATEUSER = "12345678912" },
+                new AssetsType { TYPEID = "12", EXPIRYDATE = 10, EXPIRYDATEUNIT = 1, ISENABLE = 1, NAME = "内存条", TLEVEL = 2, PARENTTYPEID = "02", CREATEUSER = "12345678912" },
+                new AssetsType { TYPEID = "13", EXPIRYDATE = 10, EXPIRYDATEUNIT = 1, ISENABLE = 1, NAME = "键盘", TLEVEL = 2, PARENTTYPEID = "03", CREATEUSER = "12345678912" },
+                new AssetsType { TYPEID = "14", EXPIRYDATE = 10, EXPIRYDATEUNIT = 1, ISENABLE = 1, NAME = "鼠标", TLEVEL = 2, PARENTTYPEID = "03", CREATEUSER = "12345678912" },
+                new AssetsType { TYPEID = "15", EXPIRYDATE = 10, EXPIRYDATEUNIT = 1, ISENABLE = 1, NAME = "路由器", TLEVEL = 2, PARENTTYPEID = "04", CREATEUSER = "12345678912" },
+                new AssetsType { TYPEID = "16", EXPIRYDATE = 10, EXPIRYDATEUNIT = 1, ISENABLE = 1, NAME = "交换机", TLEVEL = 2, PARENTTYPEID = "04", CREATEUSER = "12345678912" },
+                new AssetsType { TYPEID = "17", EXPIRYDATE = 10, EXPIRYDATEUNIT = 1, ISENABLE = 1, NAME = "投影仪", TLEVEL = 2, PARENTTYPEID = "05", CREATEUSER = "12345678912" },
+                new AssetsType { TYPEID = "18", EXPIRYDATE = 10, EXPIRYDATEUNIT = 1, ISENABLE = 1, NAME = "多方通话仪", TLEVEL = 2, PARENTTYPEID = "05", CREATEUSER = "12345678912" },
+                new AssetsType { TYPEID = "19", EXPIRYDATE = 10, EXPIRYDATEUNIT = 1, ISENABLE = 1, NAME = "打印机", TLEVEL = 2, PARENTTYPEID = "05", CREATEUSER = "12345678912" },
+                new AssetsType { TYPEID = "20", EXPIRYDATE = 10, EXPIRYDATEUNIT = 1, ISENABLE = 1, NAME = "考勤机", TLEVEL = 2, PARENTTYPEID = "05", CREATEUSER = "12345678912" },
+                new AssetsType { TYPEID = "21", EXPIRYDATE = 10, EXPIRYDATEUNIT = 1, ISENABLE = 1, NAME = "碎纸机", TLEVEL = 2, PARENTTYPEID = "05", CREATEUSER = "12345678912" }
                 );
             context.AssLocations.AddOrUpdate(
                 new AssLocation { LOCATIONID = "01", NAME = "嘉兴", ISENABLE = 1, MANAGER = "12345678912" },
                 new AssLocation { LOCATIONID = "02", NAME = "上海", ISENABLE = 1, MANAGER = "12345678913" },
+                new AssLocation { LOCATIONID = "04", NAME = "杭州", ISENABLE = 0, MANAGER = "12345678913" },
                 new AssLocation { LOCATIONID = "03", NAME = "郑州", ISENABLE = 1, MANAGER = "12345678914" }
                 );
             context.Assetss.AddOrUpdate(
@@ -56,7 +72,7 @@ namespace SMOSEC.Infrastructure.Migrations
                 new Assets { ASSID = "AS2018041103", BUYDATE = DateTime.Now, CURRENTUSER = "", EXPIRYDATE = DateTime.Now.AddYears(2), IMAGE = "2014", LOCATIONID = "03", CREATEDATE = DateTime.Now, CREATEUSER = "12345678912", MODIFYDATE = DateTime.Now, MODIFYUSER = "12345678912", TYPEID = "01", STATUS = 0, NAME = "联想E997", UNIT = "台", PRICE = 5200, SN = "2003" },
                 new Assets { ASSID = "AS2018041104", BUYDATE = DateTime.Now, CURRENTUSER = "", EXPIRYDATE = DateTime.Now.AddYears(2), IMAGE = "2014", LOCATIONID = "01", CREATEDATE = DateTime.Now, CREATEUSER = "12345678912", MODIFYDATE = DateTime.Now, MODIFYUSER = "12345678912", TYPEID = "01", STATUS = 0, NAME = "联想E996", UNIT = "台", PRICE = 5300, SN = "2004" },
                 new Assets { ASSID = "AS2018041105", BUYDATE = DateTime.Now, CURRENTUSER = "", EXPIRYDATE = DateTime.Now.AddYears(2), IMAGE = "2014", LOCATIONID = "02", CREATEDATE = DateTime.Now, CREATEUSER = "12345678912", MODIFYDATE = DateTime.Now, MODIFYUSER = "12345678912", TYPEID = "01", STATUS = 0, NAME = "联想E995", UNIT = "台", PRICE = 5400, SN = "2005" },
-                new Assets { ASSID = "AS2018041106", BUYDATE = DateTime.Now, CURRENTUSER = "", EXPIRYDATE = DateTime.Now.AddYears(2), IMAGE = "2014", LOCATIONID = "01", CREATEDATE = DateTime.Now, CREATEUSER = "12345678912", MODIFYDATE = DateTime.Now, MODIFYUSER = "12345678912", TYPEID = "01", STATUS = 0, NAME = "联想E994", UNIT = "台", PRICE = 5500, SN = "2006" }
+                new Assets { ASSID = "AS2018041106", BUYDATE = DateTime.Now, CURRENTUSER = "", EXPIRYDATE = DateTime.Now.AddYears(2), IMAGE = "2014", LOCATIONID = "01", CREATEDATE = DateTime.Now, CREATEUSER = "12345678912", MODIFYDATE = DateTime.Now, MODIFYUSER = "12345678912", TYPEID = "01", STATUS = 0, NAME = "联想E994", UNIT = "台", PRICE = 5500, SN = "2006"}
                 );
             context.AssTemplates.AddOrUpdate(
                 new AssTemplate { TEMPLATEID = "AT20180401", IMAGE = "2014", MANAGER = "12345678912", NAME = "电脑", NOTE = "备注", PRICE = 5000, SPECIFICATION = "50*40", UNIT = "台" },
